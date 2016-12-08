ddd939f5e6976ff3b74f065860437ae4b906c448
Tonny <tonny.litao@gmail.com>
2016-12-08 22:42:36 +1300

    add webpack2 to bundle

---
+++ config/webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: 'build',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            use: 'babel-loader',
+            exclude: /node_modules/,
+        }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './src/index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            }
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/asset',
+            to: 'asset'
+        }], {
+            ignore: ['.DS_Store']
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false
+            }
+        }),
+    ],
+};
