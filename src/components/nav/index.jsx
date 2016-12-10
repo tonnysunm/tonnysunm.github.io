cc004c0a008a17a0740e9cc6f3dd0ff61f0e7c25
Tonny <tonny.litao@gmail.com>
2016-12-10 16:12:28 +1300

    remove all important in css

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -6,9 +6,6 @@ import styles from './nav.css'
 const Nav = ({styles, location})=>{
     
     const isWorks = location.pathname === '/works'
-    const highlight = {color:"#5EB7F1"}
-    const normal = {color:"rgba(0,0,0,0.75)"}
-
     return (
     	<nav styleName="nav">
             <Link styleName="logo" to="/">
@@ -16,8 +13,8 @@ const Nav = ({styles, location})=>{
                 <img styleName="logTitle" src="asset/img/tonnysunm.svg" />
             </Link>
             <ul>
-                <li styleName="menuHello"><Link to="/" style={!isWorks?highlight:normal}>Hello</Link></li>
-                <li><Link to="/works" style={isWorks?highlight:normal}>Works</Link></li>
+                <li styleName="menuHello"><Link to="/" styleName={!isWorks?"active":""}>Hello</Link></li>
+                <li><Link to="/works" styleName={isWorks?"active":""}>Works</Link></li>
                 <li><a href="https://medium.com/tonny-sunm">Blog</a></li>
             </ul>
     	</nav>
