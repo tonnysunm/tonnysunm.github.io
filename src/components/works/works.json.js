ecca42364e0c88c9837f4be1e3ce01eccc3d34b9
Tonny <tonny.litao@gmail.com>
2016-12-13 15:50:24 +1300

    add behance link for works page

---
+++ src/components/works/works.json.js
@@ -0,0 +1,52 @@
+export default [{
+    "title": "Cover for Volunteering NZ",
+    "img": "cover1.jpg",
+},{
+    "title": "Notes App in everyday",
+    "img": "cover2.jpg",
+},{
+    "title": "Zootopia animation",
+    "img": "cover3.jpg",
+    "behance": "https://www.behance.net/gallery/40404763/zootopia-animation"
+},{
+    "title": "WHV in NZ",
+    "img": "cover4.jpg",
+    "behance": "https://www.behance.net/gallery/42811791/WHV-IN-NEW-ZEALAND"
+},{
+    "title": "BabyHero App",
+    "img": "cover6.jpg",
+},{
+    "title": "Doouya icon",
+    "img": "cover7.jpg",
+},{
+    "title": "Robot Maker, STEM Education with LOGO",
+    "img": "cover8.jpg",
+},{
+    "title": "Kids Grow, App+Device",
+    "img": "cover9.jpg",
+},{
+    "title": "37°C App+Device",
+    "img": "cover10.jpg",
+    "behance": "https://www.behance.net/gallery/38702587/37-thermometer"
+},{
+    "title": "One address website",
+    "img": "cover11.jpg",
+    "behance": "https://www.behance.net/gallery/38734071/a-new-address-system-APP"
+},{
+    "title": "BabyBoat App+Device",
+    "img": "cover12.jpg",
+    "behance": "https://www.behance.net/gallery/38696549/Baby-boat-APP"
+},{
+    "title": "Bussiness Card design",
+    "img": "cover13.jpg",
+    "behance": "https://www.behance.net/gallery/41744905/robot-maker"
+},{
+    "title": "Website",
+    "img": "cover14.jpg",
+},{
+    "title": "Medical record App",
+    "img": "cover15.jpg",
+},{
+    "title": "MengBox App, Origami DIY",
+    "img": "cover16.jpg",
+}]
