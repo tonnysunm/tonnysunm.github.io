24d33968d945150da471d2b9de59f85aac487ed3
Tonny <tonny.litao@gmail.com>
2017-03-03 11:56:53 +1300

    add Go App works

--- src/components/works/works.json.js
+++ src/components/works/works.json.js
@@ -1,85 +1,89 @@
 export default [{
+    "title": "Go Bus App",
+    "img": "go_bus.jpg",
+    "dribbble": "https://dribbble.com/shots/3334469-Go-bus-APP"
+}, {
     "title": "ANZ App Redesign",
     "img": "ANZ.gif",
     "dribbble": "https://dribbble.com/shots/3313230-ANZ-bank-app"
-},{
+}, {
     "title": "Dribble Redesign",
     "img": "cover27.gif",
     "dribbble": "https://dribbble.com/shots/3189567-Rooster-Calendar"
-},{
+}, {
     "title": "Cinema App Redesign",
     "img": "cover26.jpg",
     "dribbble": "https://dribbble.com/shots/3304161-Event-Cinema"
-},{
+}, {
     "title": "Doorkeeper at Cuba Street",
     "img": "cover24.jpg",
     "dribbble": "https://dribbble.com/shots/3256913-Doorman"
-},{
+}, {
     "title": "Retail App",
     "img": "cover25.jpg",
     "dribbble": "https://dribbble.com/shots/3229896-Red-Cross-App"
-},{
+}, {
     "title": "Manager",
     "img": "cover23.jpg",
     "dribbble": "https://dribbble.com/shots/3232569-Avatar-Manager"
-},{
+}, {
     "title": "Rooster Calendar",
     "img": "cover21.jpg",
     "dribbble": "https://dribbble.com/shots/3189567-Rooster-Calendar"
-},{
+}, {
     "title": "Workstudio",
     "img": "cover22.jpg",
     "dribbble": "https://dribbble.com/shots/3194322-Workstudio"
-},{
+}, {
     "title": "Cover for Volunteering NZ",
     "img": "cover1.jpg",
-},{
+}, {
     "title": "Notes App in everyday",
     "img": "cover2.jpg",
     "github": "https://github.com/TonnyTao/Acornote"
-},{
+}, {
     "title": "Zootopia animation",
     "img": "cover3.jpg",
     "behance": "https://www.behance.net/gallery/40404763/zootopia-animation"
-},{
+}, {
     "title": "WHV in NZ",
     "img": "cover4.jpg",
     "behance": "https://www.behance.net/gallery/42811791/WHV-IN-NEW-ZEALAND"
-},{
+}, {
     "title": "BabyHero App",
     "img": "cover6.jpg",
-},{
+}, {
     "title": "Doouya icon",
     "img": "cover7.jpg",
-},{
+}, {
     "title": "Robot Maker, STEM Education with LOGO",
     "img": "cover8.jpg",
-},{
+}, {
     "title": "Kids Grow, App+Device",
     "img": "cover9.jpg",
-},{
+}, {
     "title": "37°C App+Device",
     "img": "cover10.jpg",
     "behance": "https://www.behance.net/gallery/38702587/37-thermometer"
-},{
+}, {
     "title": "One address website",
     "img": "cover11.jpg",
     "behance": "https://www.behance.net/gallery/38734071/a-new-address-system-APP"
-},{
+}, {
     "title": "BabyBoat App+Device",
     "img": "cover12.jpg",
     "behance": "https://www.behance.net/gallery/38696549/Baby-boat-APP"
-},{
+}, {
     "title": "Bussiness Card design",
     "img": "cover13.jpg",
     "behance": "https://www.behance.net/gallery/41744905/robot-maker"
-},{
+}, {
     "title": "Website",
     "img": "cover14.jpg",
-},{
+}, {
     "title": "Medical record App",
     "img": "cover15.jpg",
-},{
+}, {
     "title": "MengBox App, Origami DIY",
     "img": "cover16.jpg",
     "douban": "https://www.douban.com/note/287618328/",
